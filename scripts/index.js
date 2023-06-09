ae06ef13323ee71a7b3e8e6b12d8ffed26e2a591
Saifur Rahman <118433585+devSaifur@users.noreply.github.com>
2023-06-09 18:28:31 +0600

    adding nav transition

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,34 @@
 const nav = document.getElementById("nav");
-const navBtn = document.getElementById("menu")
+const navOpenBtn = document.querySelector(".nav-open-btn");
+const navCloseBtn = document.querySelector(".nav-close-btn");
 
-navBtn.addEventListener("click", () => {
-    nav.classList.toggle("active");
+function openNav(open, close) {
+    open.addEventListener("click", () => {
+        nav.classList.add("active");
+        close.style.display = "block";
+        open.style.display = "none";
+    })
 
-    if (navBtn.name === "menu-sharp") {
-        navBtn.name = "close-sharp";
-    } else {
-        navBtn.name = "menu-sharp";
-    } 
-})
+    close.addEventListener("click", () => {
+        nav.classList.remove("active");
+        open.style.display = "block";
+        close.style.display = "none";
+    })
+}
+
+openNav(navOpenBtn, navCloseBtn)
+
+
+
+
+// navBtn.addEventListener("click", () => {
+//     if (navBtn.name === "menu-sharp") {
+//         navBtn.name = "close-sharp";
+
+//         nav.classList.toggle("active");
+//     } else {
+//         navBtn.name = "menu-sharp";
+
+//         nav.classList.toggle("active");
+//     } 
+// })
