bc3351713e2d96f86a666b6b5b94e73e2ee3c751
Saifur Rahman <118433585+devSaifur@users.noreply.github.com>
2023-07-19 21:49:08 +0600

    fix formatting

--- scripts/index.js
+++ scripts/index.js
@@ -3,32 +3,29 @@ const navOpenBtn = document.querySelector(".nav-open-btn");
 const navCloseBtn = document.querySelector(".nav-close-btn");
 
 function openNav(open, close) {
-    open.addEventListener("click", () => {
-        nav.classList.add("active");
-        close.style.display = "block";
-        open.style.display = "none";
-    })
+  open.addEventListener("click", () => {
+    nav.classList.add("active");
+    close.style.display = "block";
+    open.style.display = "none";
+  });
 
-    close.addEventListener("click", () => {
-        nav.classList.remove("active");
-        open.style.display = "block";
-        close.style.display = "none";
-    })
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+    open.style.display = "block";
+    close.style.display = "none";
+  });
 }
 
-openNav(navOpenBtn, navCloseBtn)
-
-
-
+openNav(navOpenBtn, navCloseBtn);
 
 // navBtn.addEventListener("click", () => {
-//     if (navBtn.name === "menu-sharp") {
-//         navBtn.name = "close-sharp";
+//   if (navBtn.name === "menu-sharp") {
+//     navBtn.name = "close-sharp";
 
-//         nav.classList.toggle("active");
-//     } else {
-//         navBtn.name = "menu-sharp";
+//     nav.classList.toggle("active");
+//   } else {
+//     navBtn.name = "menu-sharp";
 
-//         nav.classList.toggle("active");
-//     } 
-// })
+//     nav.classList.toggle("active");
+//   }
+// });
